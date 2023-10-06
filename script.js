7d6dedcd8a35aa14a1c33b6b33de5fdc8903aebe
KataKappel <kappelkatalin@gmail.com>
2023-10-06 19:03:34 +0200

    added new scipt.js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+console.log("hello world")
 
+function domManipulation() {
+    const rootElement = document.querySelector("#root")
+    console.log(rootElement)
+    rootElement.innerHTML = "this is done by dom manipulation"
+}
+
+function logClick() {
+    console.log('clicked')
+}
+
+let tickCount = 0
+
+function logTick() {
+    console.log(`tick ${tickCount}`)
+    tickCount++
+}
+
+window.addEventListener('load', domManipulation)
+window.addEventListener('click', logClick)
+
+window.setInterval(logTick, 1000)
 
 
 
